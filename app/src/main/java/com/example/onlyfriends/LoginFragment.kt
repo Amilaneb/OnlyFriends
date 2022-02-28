72857427f1a72f18cbbec42bf7cbd1a21d2a055b
Yanis <yanoines12@gmail.com>
2022-02-28 11:12:53 +0100

    commit for stash

--- app/src/main/java/com/example/onlyfriends/LoginFragment.kt
+++ app/src/main/java/com/example/onlyfriends/LoginFragment.kt
@@ -6,16 +6,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.onlyfriends.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
 
     lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth;
     var interactor: LoginActivityFragmentInteraction? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // ...
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if(currentUser != null){
+            Toast.makeText(requireActivity(),"You Signed In successfully",Toast.LENGTH_LONG).show();
+
+        }else {
+            Toast.makeText(requireActivity(),"You Didnt signed in",Toast.LENGTH_LONG).show();
+        }
+
     }
 
     override fun onAttach(context: Context) {
--- app/src/main/java/com/example/onlyfriends/UserActivity.kt
+++ app/src/main/java/com/example/onlyfriends/UserActivity.kt
@@ -1,8 +1,15 @@
 package com.example.onlyfriends
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.onlyfriends.databinding.ActivityUserBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 interface LoginActivityFragmentInteraction{
     fun showLogin()
@@ -17,6 +24,25 @@ class UserActivity : AppCompatActivity(),  LoginActivityFragmentInteraction {
 
         binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
 
         val fragment = RegisterFragment()
         supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, fragment).commit()
