5f67af9d2d25168ed22f43f90c2a1e2f885b1746
Alexandre Deluc <alexandre.deluc07@gmail.com>
2022-02-28 18:00:00 +0100

    Ajout appareil photo et accès à la galerie

--- app/src/main/java/com/example/onlyfriends/Navigation/AddFragment.kt
+++ app/src/main/java/com/example/onlyfriends/Navigation/AddFragment.kt
@@ -1,10 +1,14 @@
 package com.example.onlyfriends.Navigation
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.onlyfriends.databinding.FragmentAccountBinding
 import com.example.onlyfriends.databinding.FragmentAddBinding
 
@@ -17,5 +21,26 @@ class AddFragment : Fragment() {
     ): View? {
         binding = FragmentAddBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cameraImg.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 10)
+        }
+
+        binding.galeryImg.setOnClickListener {
+            chooseImageGallery()
+        }
+    }
+    private fun chooseImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_CHOOSE)
+    }
+
+    companion object {
+        private val IMAGE_CHOOSE = 1000;
     }
 }
