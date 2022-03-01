681f64c8cabead898d9aa40ad08a00fa9f64fa90
Yanis <yanoines12@gmail.com>
2022-03-01 09:25:06 +0100

    main réparé

--- app/src/main/java/com/example/onlyfriends/MainActivity.kt
+++ app/src/main/java/com/example/onlyfriends/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.onlyfriends
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.onlyfriends.Navigation.AccountFragment
 import com.example.onlyfriends.Navigation.HomeFragment
 import com.example.onlyfriends.Navigation.SearchFragment
@@ -9,8 +9,36 @@ import com.example.onlyfriends.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NavigateTo()
+    }
+
+    private fun NavigateTo(){
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val accountFragment =  AccountFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, homeFragment).commit()
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit()
+                    true
+                }
+                R.id.search -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, searchFragment).commit()
+                    true
+                }
+                R.id.account -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, accountFragment).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
